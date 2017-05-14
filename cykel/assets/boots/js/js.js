0c149d486dee1ad8235994ea1035e26dfe3b31b6
birkw <william@solenberg.org>
2017-05-14 09:38:30 +0200

    fix
    fix

--- cykel/assets/boots/js/js.js
+++
@@ -1,7 +0,0 @@
-$('#btnClick').on('click',function(){
-    if($('#1').css('display')!='none'){
-    $('#2').html('Here is my dynamic content').show().siblings('div').hide();
-    }else if($('#2').css('display')!='none'){
-        $('#1').show().siblings('div').hide();
-    }
-});
