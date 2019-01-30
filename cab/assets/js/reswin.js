00670d37339e1a73763300ddc1bc46b6d8055357
birkw <william@solenberg.org>
2019-01-30 21:32:32 +0100

    Fix
    Fix

--- cab/assets/js/reswin.js
+++
@@ -1,9 +0,0 @@
-function resize()
-    {
-        var heights = window.innerHeight;
-        document.getElementById("first").style.height = heights + "px";
-    }
-    resize();
-    window.onresize = function() {
-        resize();
-};
